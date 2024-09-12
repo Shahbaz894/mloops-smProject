ed1d8ac63df2a1610093b491b348423c2d3c4ab6
shahbaz <shahbazzulfiqar894@gmail.com>
2024-09-12 16:04:59 +0500

    intial commit

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,17 @@
 # https://github.com/Shahbaz894/mloops-smProject.git
 
 
+
 # clone repository   git clone https://github.com/Shahbaz894/mloops-smProject.git
+
+
+
+
+
+# import dagshub
+# dagshub.init(repo_owner='Shahbaz894', repo_name='mloops-smProject', mlflow=True)
+
+# import mlflow
+# with mlflow.start_run():
+#   mlflow.log_param('parameter name', 'value')
+#   mlflow.log_metric('metric name', 1)
